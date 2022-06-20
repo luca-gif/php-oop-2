980f6140f8edd632736f1b89ed32cb31486d9bcf
luca-gif <luca.89m@gmail.com>
2022-06-20 17:17:07 +0200

    inserito sconto

--- RegisteredUser.php
+++ RegisteredUser.php
@@ -5,23 +5,37 @@ require_once __DIR__ . '/User.php';
 class RegisteredUser extends User
 {
 
-  public $creditCard;
+  private $cardNumber;
+  private $cardValidity;
 
 
-  public function __construct($_name, $_lastname, $_creditCard)
+  public function __construct($_name, $_lastname, $_cardNumber, $_cardValidity)
   {
 
-    $this->creditCard = $_creditCard;
+    $this->cardNumber = $_cardNumber;
+    $this->cardValidity = $_cardValidity;
+
+    $this->setDiscount();
 
     parent::__construct($_name, $_lastname);
   }
 
-  public function setDiscount($discount)
+  public function setDiscount()
   {
     $this->discount = 20;
   }
+
+  public function setcardNumber($_cardNumber)
+  {
+    $this->cardNumber = $_cardNumber;
+  }
+
+  public function getcardNumber()
+  {
+    $this->cardNumber;
+  }
 }
 
-$paolo = new RegisteredUser('Paolo', 'Rossi', 0151515);
+$paolo = new RegisteredUser('Paolo', 'Rossi', 0151515, '12-02-2024');
 
 var_dump($paolo);
--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
-/* require __DIR__ . '/Prodotto.php';
-require __DIR__ . '/Cibo.php';
-require __DIR__ . '/Cuccia.php';
-require __DIR__ . '/Giochi.php';
-require __DIR__ . '/User.php';
-require __DIR__ . '/RegisteredUser.php'; */
+require_once __DIR__ . '/Prodotto.php';
+require_once __DIR__ . '/Cibo.php';
+require_once __DIR__ . '/Cuccia.php';
+require_once __DIR__ . '/Giochi.php';
+require_once __DIR__ . '/User.php';
+require_once __DIR__ . '/RegisteredUser.php';
